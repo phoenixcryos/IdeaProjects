76773c7c5136cc7b6def25473a742c6a07611386
phoenixcryos <rsharp32@student.cccs.edu>
2023-09-26 21:17:08 -0600

    Exam1

---
+++ Exam1/src/PrintMessage.java
@@ -0,0 +1,13 @@
+/*
+Author: Richard Sharp
+Date: 9/26/2023
+Class: CSC1060
+Description: Exam1 part 1
+ */
+
+public class PrintMessage {
+
+    public static void main(String[] args) {
+        System.out.println("This is my first programming test");
+    }
+}
